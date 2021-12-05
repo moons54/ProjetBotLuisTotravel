3819a57b36305d14ab5ccc6530ea5ca85d623deb
aurelien mimouni <aurelien.mimouni@gmail.com>
2021-12-05 21:35:29 +0100

    'modif cle config'

--- config.py
+++ config.py
@@ -13,9 +13,9 @@ class DefaultConfig:
     APP_ID = os.environ.get("MicrosoftAppId", "")
     APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
     LUIS_APP_ID = os.environ.get("LuisAppId", "039dc8b4-511a-45a2-b144-c8fb30795859")
-    LUIS_API_KEY = os.environ.get("LuisAPIKey", "6bbb51e473e3469baddc39e0830e5af4")
+    LUIS_API_KEY = os.environ.get("LuisAPIKey", "bffff56526da40759b2730ba3bb6b800")
     # LUIS endpoint host name, ie "westus.api.cognitive.microsoft.com"
-    LUIS_API_HOST_NAME = os.environ.get("LuisAPIHostName", "https://mybotluisoc-authoring.cognitiveservices.azure.com/")
+    LUIS_API_HOST_NAME = os.environ.get("LuisAPIHostName", "https://mybotluisoc.cognitiveservices.azure.com/")
     APPINSIGHTS_INSTRUMENTATION_KEY = os.environ.get(
         "AppInsightsInstrumentationKey", "b6dc2b99-2c28-4b4f-87fe-4fa764ed78eb"
     )

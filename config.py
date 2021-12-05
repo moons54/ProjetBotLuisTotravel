12ae8f2064230cd02b1ababab8e9ab0f1e319fd5
aurelien mimouni <aurelien.mimouni@gmail.com>
2021-12-05 23:04:32 +0100

    'modif cle app'

--- config.py
+++ config.py
@@ -10,8 +10,8 @@ class DefaultConfig:
     """Configuration for the bot.with mdp"""
 
     PORT = 3978
-    APP_ID = os.environ.get("MicrosoftAppId", "")
-    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
+    APP_ID = os.environ.get("MicrosoftAppId", "52d93e78-62f5-4064-ad97-09f6dffd0316")
+    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "nolss2d4320wslec4azej2mqruuhk8zlwstcoluj")
     LUIS_APP_ID = os.environ.get("LuisAppId", "bf5b04b9-91bc-41d4-926e-ca898e4e540d")
     LUIS_API_KEY = os.environ.get("LuisAPIKey", "51b95514a327424594da58bfb0319d30")
     # LUIS endpoint host name, ie "westus.api.cognitive.microsoft.com"
@@ -19,3 +19,5 @@ class DefaultConfig:
     APPINSIGHTS_INSTRUMENTATION_KEY = os.environ.get(
         "AppInsightsInstrumentationKey", "b6dc2b99-2c28-4b4f-87fe-4fa764ed78eb"
     )
+
+
